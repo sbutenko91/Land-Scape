e3afd0ab10592050364fc432c975ca098022f398
Fullstack Developer <dedicatedbusinessunit@gmail.com>
2017-11-03 20:12:11 +0200

    fixed path

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-    res.render('/homepage/index');
+    res.render('index');
 });
 
 
